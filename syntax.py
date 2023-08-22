a0ec3b53860cba3288b5d032939ec75a6126c491
netflams <segope44@gmail.com>
2023-08-22 16:15:37 +0100

    initialize python series

---
+++ syntax.py
@@ -0,0 +1,39 @@
+# This is my first time with a programming language
+
+"""
+print("Hey it is my birthday line 5")
+    This is another type of comment
+"""
+
+# print("Hey it is my birthday line 2")
+
+
+#variables
+# x = 55
+# print(type(x))
+
+# y = "John"
+# print(y)
+
+x = float(3)
+# x = str(3)
+# x = "3"
+print(x)
+print(type(x))
+
+a2h_5 = 6
+print(a2h_5)
+
+x, y, z = 6, 5, 4
+
+fruits = ["apple", "banana", "cherry"]
+x, y, 
+z = fruits
+
+x = "Python"
+y = "is"
+z = "awesome"
+print(x, y, z)
+
+def main ():
+    x = 15
